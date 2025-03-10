8955ca4438a40c0d9a28d4aee173419ba8c88ab0
marufabiva <marufabiva56@gmail.com>
2025-03-10 21:15:13 +0600

    added index.html

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const menu = document.querySelector("nav ul");
+    menu.classList.toggle("active"); // Toggle the "active" class to show/hide the menu
+}
+
+<script>
+    function toggleMenu() {
+        const menu = document.querySelector('nav ul');
+        menu.classList.toggle('active');
+    }
+</script>
